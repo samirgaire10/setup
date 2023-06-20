37bc54b2e4000d0ace31c5b0a677a1e09bbdba52
samir gaire // ガイレサミル <118608337+samirgaire10@users.noreply.github.com>
2023-06-20 14:14:43 +0900

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -12,6 +12,9 @@ echo ---------------------------------------------------------------
 echo start updating system
 update
 
+echo installing costom tkinter (python)
+sudo pacman -S tk
+
 echo installing rpi imager
 rpi-imager
 
